af203430b099586830f135d256bf9406b691b39b
Turyan Azizov <turyanazizov13@gmail.com>
2022-03-18 15:23:22 +0400

    Blog Project hazirdir

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,27 @@
-from django.shortcuts import render,redirect
-from . models import Blog,Comment
+from django.shortcuts import render, redirect
+from . models import Blog, Comment
+
 
 def blog(request):
-    blogs=Blog.objects.all()
-    context={
-        'blogs':blogs,
+    blogs = Blog.objects.all()
+    context = {
+        'blogs': blogs,
     }
-    return render(request,'blog/blog.html',context=context)
+    return render(request, 'blog/blog.html', context=context)
 
-def single_post(request,name):
-    blog=Blog.objects.all().filter(creator_name=name).first()
-    # comments=Comment.objects.filter(status='Approve')
-    comments=Blog.objects.all().filter(creator_name=name).first().koments.filter(status='Approve')
-    context={
-        'blog':blog,
-        'comments':comments
+
+def single_post(request, name):
+    blog = Blog.objects.all().filter(creator_name=name).first()
+    comments = Blog.objects.all().filter(creator_name=name).first().koments.filter(status='Approve')
+    context = {
+        'blog': blog,
+        'comments': comments
     }
     if request.method == "POST":
         Comment.objects.create(
+            blog=Blog.objects.all().filter(creator_name=name).first(),
             text=request.POST.get("text"),
             name=request.POST.get("name"),
             email=request.POST.get("email"),
-            )
-    return render(request,'blog/single-post.html',context=context)
+        )
+    return render(request, 'blog/single-post.html', context=context)
